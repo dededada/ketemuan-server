084337a0793a4333edbe91286c07f880b22544bc
Khairul Annas <dededada2007@gmail.com>
2016-01-11 14:48:11 +0700

    add socket connction

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
-var express = require('express');
+"use strict"
+
+var express = require('express'),
+    http = require('http');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 var uuid = require('node-uuid');
 
-var listActiveRoom = []
+var listActiveRoom = {}
 
-var Room = function(uuid){
-  this.uuid=uuid;
-  this.user=[];
+class Room {
+  constructor(uuid){
+    this.uuid=uuid;
+    this.user={};
+  }
+  addUser(id){
+    this.user[id] = new User(id);
+  }
+  removeUser(id){
+    delete user[id];
+  }
 };
-Room.prototype.addUser = function(id){this.user.push(id)};
+
+class User{
+  constructor(id){
+    this.id = id;
+    this.name = "";
+    this.location = {};
+    this.location.lat = 0;
+    this.location.long = 0;
+  }
+}
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('index.html');
 });
 
 app.get('/link', function (req, res){
   var uniqueId = uuid.v1();
-  room = new Room(uniqueId);
-  room.addUser(req.query.id);
-  listActiveRoom.push(room);
+  var room = new Room(uniqueId);
+  //room.addUser(req.query.id);
+  // TODO: user must be unique
+  listActiveRoom[uniqueId] = room;
+  room.link = 'http://'+req.headers.host+'/room/'+uniqueId;
   res.send(room);
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('join',function(userId, roomId){
+      //console.log('join requested by '+userId+' for room '+roomId);
+      socket.join(roomId);
+      socket.room = roomId;
+      socket.id = userId;
+      listActiveRoom[roomId].addUser(userId);
+      console.log(listActiveRoom);
+  });
 });
 
-app.listen(3000, function () {
+io.on('disconnect', function(socket){
+  socket.leave(socket.room);
+  listActiveRoom[socket.room].removeUser(socket.id);
+  console.log(listActiveRoom);
+});
+
+server.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
 
